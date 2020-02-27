4475ae3ed921d9759a28ad14cef3e1f92d0e05f5
soniagitao <soniagita.26@gmail.com>
2020-02-27 07:25:46 +0700

    main activity

--- app/src/main/java/id/putraprima/skorbola/MainActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MainActivity.java
@@ -1,21 +1,73 @@
 package id.putraprima.skorbola;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int GALLERY_REQUEST_CODE = 1 ;
+    private static final String TAG = MainActivity.class.getCanonicalName();
+    private ImageView avatarImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        avatarImage = findViewById(R.id.image);
+
         //TODO
         //Fitur Main Activity
         //1. Validasi Input Home Team
         //2. Validasi Input Away Team
-        //3. Ganti Logo Home Team
+
         //4. Ganti Logo Away Team
-        //5. Next Button Pindah Ke MatchActivity
+
     }
+
+    //3. Ganti Logo Home Team
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if (data != null) {
+                try {
+                    Uri imageUri = data.getData();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    avatarImage.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void handleChangeAvatar(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+    }
+
+    //5. Next Button Pindah Ke MatchActivity
+    public void handleMatchActivity(View view) {
+        Intent intent = new Intent(this, MatchActivity.class);
+        startActivity(intent);
+    }
+
 }
